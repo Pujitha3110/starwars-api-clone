c23dff506a834a9df6663ab9431971a3032dc03f
Pujitha3110 <pujithamaddipati2@gmail.com>
2026-07-05 12:16:25 +0530

    Initial project setup with Express and MongoDB

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Default Route
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to Star Wars API Clone 🚀"
+    });
+});
+
+module.exports = app;
